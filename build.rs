62010b56bcf2219b336a0472908686d5c04f4c32
GitGhillie <jillisnoordhoek@gmail.com>
2023-09-27 15:23:53 +0200

    build

--- build.rs
+++ build.rs
@@ -1,7 +1,25 @@
 fn main() {
-    #[cfg(target_os = "linux")]
+    #[cfg(all(target_os = "linux", target_arch = "x86"))]
+    {
+        println!("cargo:rustc-link-search=/usr/lib/i386-linux-gnu/fmod-api/");
+        println!("cargo:rustc-env=LD_LIBRARY_PATH=/usr/lib/i386-linux-gnu/fmod-api/");
+    }
+
+    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
     {
         println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu/fmod-api/");
         println!("cargo:rustc-env=LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/fmod-api/");
+    }
+
+    #[cfg(all(target_os = "linux", target_arch = "arm"))]
+    {
+        println!("cargo:rustc-link-search=/usr/lib/arm-linux-gnueabihf/fmod-api/");
+        println!("cargo:rustc-env=LD_LIBRARY_PATH=/usr/lib/arm-linux-gnueabihf/fmod-api/");
+    }
+
+    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+    {
+        println!("cargo:rustc-link-search=/usr/lib/aarch64-linux-gnu/fmod-api/");
+        println!("cargo:rustc-env=LD_LIBRARY_PATH=/usr/lib/aarch64-linux-gnu/fmod-api/");
     }
 }
