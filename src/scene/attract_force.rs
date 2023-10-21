f9a6fa6c2a1ada670ea422066dbb07171a6fe8d6
GitGhillie <jillisnoordhoek@gmail.com>
2023-10-21 21:10:54 +0200

    Attract to aim location

--- src/scene/attract_force.rs
+++ src/scene/attract_force.rs
@@ -9,13 +9,23 @@ impl Plugin for AttractPlugin {
     }
 }
 
-fn attract(mut ext_forces: Query<(&mut ExternalForce, &mut GlobalTransform)>) {
+fn attract(
+    mouse_button_input: Res<Input<MouseButton>>,
+    cam_transform: Query<&GlobalTransform, With<Camera>>,
+    mut ext_forces: Query<(&mut ExternalForce, &mut GlobalTransform), Without<Camera>>,
+) {
+    let cam_transform = cam_transform.get_single().ok().unwrap();
+
     for (mut ext_force, transform) in ext_forces.iter_mut() {
-        let attraction_point = Vec3::new(0.0, 0.0, 0.0);
+        let attraction_point = cam_transform.translation() + (cam_transform.forward() * 5.0);
         let source = transform.translation();
 
         let attraction_vector = attraction_point - source;
 
-        ext_force.force = attraction_vector.normalize_or_zero() * 3000.0;
+        if mouse_button_input.pressed(MouseButton::Left) {
+            ext_force.force = attraction_vector.normalize_or_zero() * 300.0;
+        } else {
+            ext_force.force = Vec3::ZERO;
+        }
     }
 }
